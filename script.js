a787d02a5f8d2591f43af09da0f328cea5842d79
Beatrize <heybeatrize@gmail.com>
2018-08-09 15:55:32 -0700

    css edits main css

--- script.js
+++ script.js
@@ -2,17 +2,15 @@
 $(window).on('scroll', function() {
   if($(window).scrollTop() >= 300) {
     $('#portfolio h4').addClass('animated');
-    
-    let li = $(".portfolio-skills li")
-    for (i=0; i < li.length; i++) {
         setTimeout(function() {
             $('.portfolio-skills li').addClass("slide");
             $('.skill-list').removeClass("hidden")
         }, 100)  
     }
-  }
+
   if($(window).scrollTop() >= 500) {  
-      $('#services h4').addClass('animated'); 
+      $('#services h4').addClass('animated');
+      $('#portfolio span').removeClass('cursor')
         let list = $('.services-list li')
         for (i = 0; i < list.length; i++) {
             (function(i) {
@@ -23,12 +21,15 @@ $(window).on('scroll', function() {
         }
   }
   if($(window).scrollTop() >= 900) {  
-      $('#social h4').addClass('animated'); 
+      $('#social h4').addClass('animated');
+      $('#services span').removeClass('cursor');
+      $('#one').removeClass('hidden').addClass('animated2');
+      $('#two').removeClass('hidden').addClass('animated3')
         let list = $('.social-list li')
         for (i = 0; i < list.length; i++) {
             (function(i) {
                 setTimeout(function() { 
-                    list[i].setAttribute("style", "animation: slidedown 0.5s ease; visibility: visible");  
+                    list[i].setAttribute("style", "animation: slidedown 0.5s ease; visibility: visible");
                 }, i*200)
             }(i))
         }
